5a2ab6744abe63c5ef536786b237c97af7e52e20
Rupinder Singh <techformation.rupinder@gmail.com>
2024-01-25 15:43:49 +0530

    two way data binding

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -8,10 +8,9 @@ import { Appointment } from '../models/appointment';
 })
 export class AppointmentListComponent {
 
-  appointment: Appointment = {
-    id: 1,
-    title: 'take dog for a walk',
-    date: new Date('2024-01-25')
-  };
+  newAppointmentTitle : string = "";
+  newAppointmentDate : Date = new Date();
+
+  appointment: Appointment[] = []
   
 }
